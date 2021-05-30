2499725eeb567e1eb6da9cd582b8dd2c574526f7
luoxuzhi <herolxz@163.com>
2021-05-30 09:12:42 +0800

    getSingerList finished

--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,10 +1,33 @@
 <template>
-  <div>singer</div>
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers"></index-list>
+  </div>
 </template>
 
 <script>
-export default {}
+import { getSingerList } from '@/service/singer'
+import IndexList from '@/components/base/index-list/index-list'
+
+export default {
+  data() {
+    return {
+      singers: [],
+    }
+  },
+  components: { IndexList },
+  async created() {
+    const result = await getSingerList()
+    this.singers = result.singers
+    console.log('result :', result)
+  },
+}
 </script>
 
 <style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
 </style>
